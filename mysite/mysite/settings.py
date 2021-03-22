4bf64fd2d4af23c924ea3cfd353ceffcbad78484
yasina2020 <8459055287wudi@gmail.com>
2021-03-22 12:47:10 +0800

    1

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_test',
-        'USER': 'root',
-        'PASSWORD': 'wu',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {'isolation_level': None}
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'django_test',
+#         'USER': 'root',
+#         'PASSWORD': 'wu',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'OPTIONS': {'isolation_level': None}
+#     }
+# }
 
 
 # Password validation
